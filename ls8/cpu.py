11f7b72682f01db89f368e455b16aa897de0024f
Jesse Theisen <theiseja@gmail.com>
2020-04-23 20:56:13 -0500

    Implements CPU constructor

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,19 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.reg = [0] * 8 # register
+        self.ram = [0] * 256
 
     def load(self):
         """Load a program into memory."""
@@ -16,19 +23,43 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        try:
+            address = 0
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    comment_split = line.strip().split('#')
+                    value = comment_split[0].strip()
+                    if value == '':
+                        continue
+                    instruction = int(value, 2)
+                    # populate memory array
+                    self.ram[address] = instruction
+                    address += 1
+
+        except:
+            print('Cant find file')
+            sys.exit(2)
+
+    def ram_read(self, mar):
+        mdr = self.ram[mar]
+        return mdr
+    
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,4 +93,21 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while True:
+            opcode = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if opcode == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif opcode == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif opcode == MUL:
+                self.alu(opcode, operand_a, operand_b)
+                self.pc += 3
+            elif opcode == HLT:
+                sys.exit(0)
+            else:
+                print(f'Did not work')
+                sys.exit(1)
